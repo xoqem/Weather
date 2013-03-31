ea8ea714da8e58b7d12c50227bc59edf024d6521
xoqem <sinfree@gmail.com>
2013-03-31 15:42:49 -0700

    removes unused grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -120,7 +120,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-ember-templates');
 
   // default task
   grunt.registerTask('default', ['clean', 'jshint', 'concat', 'copy', 'uglify', 'cssmin', 'compress', 'sleep']);
