1d7b0b98fe2a81a2c65b757771af054cf793ef5d
xoqem <sinfree@gmail.com>
2013-04-01 00:11:14 -0700

    makes model a getter to solve creation order bug

--- js/collections/forecasts.js
+++ js/collections/forecasts.js
@@ -2,7 +2,16 @@
   'use strict';
 
   var ForecastList = Backbone.Collection.extend({
-    model: app.Forecast,
+    model: function(attrs, options) {
+      return new app.Forecast(attrs, options);
+    },
+
+    // FIXME: backbone collection is expecting a url, that it will hit to sync everytime an item
+    // is added to the collection.  This is clearly not what we are wanting to do here when we get
+    // a new forecast.  This makes me think that collection may be the wrong type of Backbone class
+    // to represent the forecasts the user has fetched so far, as these will never be sent back to
+    // the server.
+    url: "http://example.com",
 
     comparator: function (forecast) {
       return forecast.get('latitude');
