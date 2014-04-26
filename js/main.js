01e26582ac19a672d14f0f7aaf9affd0d66cf7ec
xoqem <sinfree@gmail.com>
2014-04-26 01:51:37 -0700

    Update dependencies

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 require.config({
   paths: {
     templates : '../templates',
-    text: '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.5/text.min',
-    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min',
-    underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min',
-    backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min'
+    text: '//cdnjs.cloudflare.com/ajax/libs/require-text/2.0.10/text.min',
+    jquery: '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min',
+    underscore: '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min',
+    backbone: '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min'
   },
   shim: {
     underscore: {
